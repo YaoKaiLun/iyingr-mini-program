89d2ea96b4fbb45333c418728d6cc138186b2085
allen <yaokailun@ifanr.com>
2017-12-24 21:28:53 +0800

    prettify code

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -1,32 +1,31 @@
 const articleInfo = {
   title: '特斯拉卡车发布',
-  category: '科技',
   poster: 'https://cloud-minapp-1131.cloud.ifanrusercontent.com/1eNDLNIcksPYFzMl.jpg',
   content: '双十一已经过去一周了，包裹收齐了吗？有没有觉得快递费用更贵了？越来越大的物流需求，需要企业在车辆和人力上支出更多，而这消耗的每一分钱，最终都需要你我这样的消费者买单。想要省下这一大笔钱，与其盼着学会控制自己不去剁手，不如盼着新的科技革新。美国加州 16 日下午（北京 17 日上午），特斯拉正式揭开了自己电动卡车的面纱，爱范儿旗下董车会（微信搜索：董车会）作为现场唯一的国内汽车媒体，为各位 0 距离解密这台卡车背后的生财之道。P.S.发布会最后，埃隆马斯克突然拿出了把全世界碾压在脚下的火红怪兽，你赚了钱干啥他都给你想好了。',
   created_at: '2017-11-11'
 }
 
 let id = ''
+
 Page({
   data: {
     article: {},
   },
+
   onLoad: function(option) {
     id = option.id
+    this.getArticle(id)
+  },
+
+  getArticle: function(id) {
     this.setData({article: articleInfo})
   },
-  onShareAppMessage: function (res) {
-    let article = this.data.article
+
+  onShareAppMessage: function (options) {
     return {
-      title: article.title,
-      imageUrl: article.poster,
+      title: this.data.article.title,
+      imageUrl: this.data.article.poster,
       path: `pages/detail/index?id=${id}`,
-      success: function (res) {
-        // 转发成功
-      },
-      fail: function (res) {
-        // 转发失败
-      }
     }
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,44 +1,44 @@
+const pageLimit = 4
+let isEnd = false
 const firstPage = [{
   id: '1',
   title: '装修秘诀',
-  digest: '文艺气息爆棚的精致白色现代家',
-  poster: 'https://cloud-minapp-1131.cloud.ifanrusercontent.com/1eNDc3TmmbboTqKa.jpg',
+  description: '文艺气息爆棚的精致白色现代家',
+  cover: 'https://cloud-minapp-1131.cloud.ifanrusercontent.com/1eNDc3TmmbboTqKa.jpg',
   collect: 0
 }, {
   id: '2',
   title: '咖啡指南',
-  digest: '咖啡制作终极指南',
-  poster: 'https://cloud-minapp-1131.cloud.ifanrusercontent.com/1eNDYBVedtSdnNRR.jpeg',
+  description: '咖啡制作终极指南',
+  cover: 'https://cloud-minapp-1131.cloud.ifanrusercontent.com/1eNDYBVedtSdnNRR.jpeg',
   collect: 0
 }, {
   id: '3',
   title: '特斯拉卡车发布',
-  digest: '你以为只是发卡车，马斯克却把世界踩在脚下',
-  poster: 'https://cloud-minapp-1131.cloud.ifanrusercontent.com/1eNDLNIcksPYFzMl.jpg',
+  description: '你以为只是发卡车，马斯克却把世界踩在脚下',
+  cover: 'https://cloud-minapp-1131.cloud.ifanrusercontent.com/1eNDLNIcksPYFzMl.jpg',
   collect: 0
 }, {
   id: '4',
   title: 'iReader Ocean 电子书评测',
-  digest: '国产千元电子书能否复制 MIUI 的辉煌？',
-  poster: 'http://images.ifanr.cn/wp-content/uploads/2017/12/Ocean23.jpg',
+  description: '国产千元电子书能否复制 MIUI 的辉煌？',
+  cover: 'http://images.ifanr.cn/wp-content/uploads/2017/12/Ocean23.jpg',
   collect: 0
 }]
-
 const lastPage = [{
   id: '5',
   title: '超新约全书',
-  digest: '一个以折磨人为乐趣的上帝',
-  poster: 'https://media.ifanrusercontent.com/media/user_files/hydrogen/45/df/45df7216505002c00b5d888f6a5137fc2088c3a4-6cf38167dde685621d472c16c88b9d9e47f85c7b.jpg',
+  description: '一个以折磨人为乐趣的上帝',
+  cover: 'https://media.ifanrusercontent.com/media/user_files/hydrogen/45/df/45df7216505002c00b5d888f6a5137fc2088c3a4-6cf38167dde685621d472c16c88b9d9e47f85c7b.jpg',
   collect: 0
 }, {
     id: '6',
     title: '2001太空漫游 2001',
-    digest: '现代科幻电影技术的里程碑',
-    poster: 'https://media.ifanrusercontent.com/media/user_files/hydrogen/d3/bd/d3bd4900a1ca1ed76e3bd39b9961084d3fe8234c-ba44bb070dd36bc3a6a0adbe1b4faa524673fe9b.jpg',
+    description: '现代科幻电影技术的里程碑',
+    cover: 'https://media.ifanrusercontent.com/media/user_files/hydrogen/d3/bd/d3bd4900a1ca1ed76e3bd39b9961084d3fe8234c-ba44bb070dd36bc3a6a0adbe1b4faa524673fe9b.jpg',
     collect: 0
 }]
-
-let isEnd = false
+const READED_ARTICLES = 'READED_ARTICLES'
 
 Page({
   data: {
@@ -46,46 +46,56 @@ Page({
     loading: false,
     loadMoreText: '加载更多'
   },
+
   onLoad: function() {
-    this.getfirstPage()
+    this.getArticles(true)
   },
-  getfirstPage: function() {
-    this.setData({
-      articles: this.addReadStatus(firstPage)
-    })
+
+  loadMore: function(event) {
+    this.getArticles()
   },
-  loadMore: function() {
-    if (!isEnd) {
-      this.setData({loading: true})
+
+  getArticles: function(isFirstPage) {
+    if (!isEnd && !this.data.loading) {
+      this.setData({ loading: true })
       setTimeout(() => {
-        this.setData({
-          articles: this.addReadStatus(firstPage.concat(lastPage)),
-          loading: false
-        })
+        if (isFirstPage) { 
+          this.setData({
+            articles: this.addReadStatus(firstPage),
+            loading: false
+          })
+        } else {
+          this.setData({
+            articles: this.addReadStatus(firstPage.concat(lastPage)),
+            loading: false
+          })
+          if (lastPage.length < pageLimit) {
+            isEnd = true
+            this.setData({ loadMoreText: '已无更多' })
+          }
+        }
       }, 1000)
-      isEnd = true
-    } else {
-      this.setData({loadMoreText: '已无更多'})
     }
   },
+
   toDetailPage: function(e) {
     let id = e.currentTarget.dataset.id
-    let index = e.currentTarget.dataset.index
-    let readedPosts = wx.getStorageSync('readedPosts')
+    let readedPosts = wx.getStorageSync(READED_ARTICLES)
 
     if (!readedPosts) {
-      wx.setStorageSync('readedPosts', [id])
+      wx.setStorageSync(READED_ARTICLES, [id])
     } else if(readedPosts.indexOf(id) == -1) {
       readedPosts.push(id)
-      wx.setStorageSync('readedPosts', readedPosts)
+      wx.setStorageSync(READED_ARTICLES, readedPosts)
     }
     this.setData({articles: this.addReadStatus(this.data.articles)})
     wx.navigateTo({
       url: `../detail/index?id=${id}`
     })
   },
+
   addReadStatus: function(movies) {
-    let readedPosts = wx.getStorageSync('readedPosts')
+    let readedPosts = wx.getStorageSync(READED_ARTICLES)
     if (!readedPosts) {
       return movies
     }
