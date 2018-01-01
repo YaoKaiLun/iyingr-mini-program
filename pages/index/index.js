01bc8bd853dc29c0206b24a818df06d4548a6eb8
allen <yaokailun@ifanr.com>
2018-01-01 10:50:31 +0800

    prettify code

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,39 +4,33 @@ const firstPage = [{
   id: '1',
   title: '装修秘诀',
   description: '文艺气息爆棚的精致白色现代家',
-  cover: 'https://cloud-minapp-1131.cloud.ifanrusercontent.com/1eNDc3TmmbboTqKa.jpg',
-  collect: 0
+  cover: 'https://cloud-minapp-1131.cloud.ifanrusercontent.com/1eNDc3TmmbboTqKa.jpg'
 }, {
   id: '2',
   title: '咖啡指南',
   description: '咖啡制作终极指南',
-  cover: 'https://cloud-minapp-1131.cloud.ifanrusercontent.com/1eNDYBVedtSdnNRR.jpeg',
-  collect: 0
+  cover: 'https://cloud-minapp-1131.cloud.ifanrusercontent.com/1eNDYBVedtSdnNRR.jpeg'
 }, {
   id: '3',
   title: '特斯拉卡车发布',
   description: '你以为只是发卡车，马斯克却把世界踩在脚下',
-  cover: 'https://cloud-minapp-1131.cloud.ifanrusercontent.com/1eNDLNIcksPYFzMl.jpg',
-  collect: 0
+  cover: 'https://cloud-minapp-1131.cloud.ifanrusercontent.com/1eNDLNIcksPYFzMl.jpg'
 }, {
   id: '4',
   title: 'iReader Ocean 电子书评测',
   description: '国产千元电子书能否复制 MIUI 的辉煌？',
-  cover: 'http://images.ifanr.cn/wp-content/uploads/2017/12/Ocean23.jpg',
-  collect: 0
+  cover: 'http://images.ifanr.cn/wp-content/uploads/2017/12/Ocean23.jpg'
 }]
 const lastPage = [{
   id: '5',
   title: '超新约全书',
   description: '一个以折磨人为乐趣的上帝',
-  cover: 'https://media.ifanrusercontent.com/media/user_files/hydrogen/45/df/45df7216505002c00b5d888f6a5137fc2088c3a4-6cf38167dde685621d472c16c88b9d9e47f85c7b.jpg',
-  collect: 0
+  cover: 'https://media.ifanrusercontent.com/media/user_files/hydrogen/45/df/45df7216505002c00b5d888f6a5137fc2088c3a4-6cf38167dde685621d472c16c88b9d9e47f85c7b.jpg'
 }, {
     id: '6',
     title: '2001太空漫游 2001',
     description: '现代科幻电影技术的里程碑',
-    cover: 'https://media.ifanrusercontent.com/media/user_files/hydrogen/d3/bd/d3bd4900a1ca1ed76e3bd39b9961084d3fe8234c-ba44bb070dd36bc3a6a0adbe1b4faa524673fe9b.jpg',
-    collect: 0
+    cover: 'https://media.ifanrusercontent.com/media/user_files/hydrogen/d3/bd/d3bd4900a1ca1ed76e3bd39b9961084d3fe8234c-ba44bb070dd36bc3a6a0adbe1b4faa524673fe9b.jpg'
 }]
 const READED_ARTICLES = 'READED_ARTICLES'
 
@@ -59,7 +53,7 @@ Page({
     if (!isEnd && !this.data.loading) {
       this.setData({ loading: true })
       setTimeout(() => {
-        if (isFirstPage) { 
+        if (isFirstPage) {
           this.setData({
             articles: this.addReadStatus(firstPage),
             loading: false
